d0dcd38ce61b963dd8683bf5ca24b577a868849d
Grant Geldenhuys <grant.geldenhuys@yahoo.com>
2020-09-25 17:46:34 +0200

    core chatbot functionality

--- Chatbot.py
+++ Chatbot.py
@@ -1,10 +1,8 @@
 import cleverbotfree.cbfree
 import sys
-
-#Speech recog
 import speech_recognition as sr
 
-r = sr.Recognizer()
+""" r = sr.Recognizer()
 with sr.Microphone() as source:
     print("Speak Anything :")
     audio = r.listen(source)
@@ -12,11 +10,10 @@ with sr.Microphone() as source:
         text = r.recognize_google(audio)
         print("You said : {}".format(text))
     except:
-        print("Sorry could not recognize what you said")
+        print("Sorry could not recognize what you said") """
 
 #Cleverbot
 cb = cleverbotfree.cbfree.Cleverbot()
-
 
 def main():
     try:
@@ -25,16 +22,27 @@ def main():
         cb.browser.close()
         sys.exit()
     while True:
+        userInput = 0
         try:
             cb.get_form()
         except:
             sys.exit()
-        userInput = input('User: ')
-        if userInput == 'quit':
-            break
-        cb.send_input(userInput)
-        bot = cb.get_response()
-        print('Cleverbot: ', bot)
+
+        r = sr.Recognizer()
+        with sr.Microphone() as source:
+            print("Speak Anything internal:")
+            audio = r.listen(source)
+        try:
+            text = r.recognize_google(audio)
+            print("You said : {}".format(text))
+            userInput = text
+            if userInput == 'quit':
+                break
+            cb.send_input(userInput)
+            bot = cb.get_response()
+            print('Cleverbot: ', bot)            
+        except:
+            print("Sorry could not recognize what you said")
     cb.browser.close()
 
     

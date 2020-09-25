2c3484f24c4587fd127af08108102654d03d3147
Grant Geldenhuys <grant.geldenhuys@yahoo.com>
2020-09-25 15:14:28 +0200

    Chatbot python code

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
 
 namespace Eve
 {
@@ -6,6 +9,23 @@ namespace Eve
     {
         static void Main(string[] args)
         {
+            WebRequest req = WebRequest.Create("https://miapi.pandorabots.com/talk");
+            string postData = "input=hi";
+
+            byte[] send = Encoding.Default.GetBytes(postData);
+            req.Method = "POST";
+            req.ContentType = "application/x-www-form-urlencoded";
+            req.ContentLength = send.Length;
+
+            Stream sout = req.GetRequestStream();
+            sout.Write(send, 0, send.Length);
+            sout.Flush();
+            sout.Close();
+
+            WebResponse res = req.GetResponse();
+            StreamReader sr = new StreamReader(res.GetResponseStream());
+            string returnvalue = sr.ReadToEnd();
+
             Console.WriteLine("Hello World!");
         }
     }

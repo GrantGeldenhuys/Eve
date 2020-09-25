f76490920527187c97fa66a15728f566b93fc0bd
Grant Geldenhuys <grant.geldenhuys@yahoo.com>
2020-09-25 16:59:27 +0200

    Added packages for speech

--- Chatbot.py
+++ Chatbot.py
@@ -1,6 +1,20 @@
 import cleverbotfree.cbfree
 import sys
 
+#Speech recog
+import speech_recognition as sr
+
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Speak Anything :")
+    audio = r.listen(source)
+    try:
+        text = r.recognize_google(audio)
+        print("You said : {}".format(text))
+    except:
+        print("Sorry could not recognize what you said")
+
+#Cleverbot
 cb = cleverbotfree.cbfree.Cleverbot()
 
 
